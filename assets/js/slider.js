4a43430535d7b56482c208010a24acdeab281463
siddiq <siddiq.aiub@gmail.com>
2020-10-04 00:27:49 +0600

    update design

---
+++ assets/js/slider.js
@@ -0,0 +1,59 @@
+$(document).ready(function () {
+
+  $(".carousel").carousel({
+    interval: true,
+    pause: true,
+    touch: true,
+      time: 1s,
+  });
+
+  $(".carousel .carousel-inner").swipe({
+    swipeLeft: function (event, direction, distance, duration, fingerCount) {
+      this.parent().carousel("next");
+    },
+    swipeRight: function () {
+      this.parent().carousel("prev");
+    },
+    threshold: 0,
+    tap: function (event, target) {
+      window.location = $(this).find(".carousel-item.active a").attr("href");
+    },
+    excludedElements: "label, button, input, select, textarea, .noSwipe"
+  });
+
+  $(".carousel .carousel-control-prev").on("click", function () {
+    $(".carousel").carousel("prev");
+  });
+
+  $(".carousel .carousel-control-next").on("click", function () {
+    $(".carousel").carousel("next");
+  });
+    
+    var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-36251023-1']);
+  _gaq.push(['_setDomainName', 'jqueryscript.net']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+    
+    try {
+  fetch(new Request("https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js", { method: 'HEAD', mode: 'no-cors' })).then(function(response) {
+    return true;
+  }).catch(function(e) {
+    var carbonScript = document.createElement("script");
+    carbonScript.src = "//cdn.carbonads.com/carbon.js?serve=CK7DKKQU&placement=wwwjqueryscriptnet";
+    carbonScript.id = "_carbonads_js";
+    document.getElementById("carbon-block").appendChild(carbonScript);
+  });
+} catch (error) {
+  console.log(error);
+}
+
+});
+
+
