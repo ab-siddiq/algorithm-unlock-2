eec90bbb0a1515f26fcd443f16e7293636da4618
siddiq <siddiq.aiub@gmail.com>
2020-10-10 02:14:00 +0600

    added some features

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,3 +13,41 @@
         $('.portfolio-menu').on('click', 'li', function() {
             $(this).addClass('active').siblings().removeClass('active');
         });
+//slick slider
+$('.autoplay').slick({
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+    
+     responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+	
+
