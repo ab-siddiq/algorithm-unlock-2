ee82f1f7801f50a158c967da7c2a51e2d5ff8498
siddiq <siddiq.aiub@gmail.com>
2020-10-11 00:39:17 +0600

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,7 +13,43 @@
         $('.portfolio-menu').on('click', 'li', function() {
             $(this).addClass('active').siblings().removeClass('active');
         });
-//slick slider
+//slick slider board member
+$('.board-member').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+    
+     responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+//slick slider sponsor
 $('.autoplay').slick({
   slidesToShow: 5,
   slidesToScroll: 1,
@@ -68,3 +104,6 @@ $('.autoplay').slick({
   }, false);
 })();
 
+$('[data-fancybox="gallery"]').fancybox({
+	// Options will go here
+});
