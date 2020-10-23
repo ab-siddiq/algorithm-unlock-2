96f910dce1b31d192c9af5ffb3d53eab16ff96b6
siddiq <siddiq.aiub@gmail.com>
2020-10-23 16:19:14 +0600

    updated some style

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,6 +14,41 @@
             $(this).addClass('active').siblings().removeClass('active');
         });
 
+//slick slider team
+$('.autoplay-team').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+    
+     responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    
+  ],
+   
+});
 //slick slider sponsor
 $('.autoplay').slick({
   slidesToShow: 5,
@@ -69,4 +104,10 @@ $('.autoplay').slick({
 
 $('[data-fancybox="gallery"]').fancybox({
 	// Options will go here
+});
+
+//counter up
+$('.counter').counterUp({
+    delay: 10,
+    time: 1000
 });
